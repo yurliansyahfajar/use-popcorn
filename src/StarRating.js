3857d5c577488c9e80ce624e7f9b7bdbdfd5622a
yurliansyahf <yurliansyahf@gmail.com>
2024-06-26 22:54:17 +0700

    create star props tobe more reusable, and also add proptype to handle props data type

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle = {
   display: "flex",
@@ -10,21 +11,38 @@ const starContainerStyle = {
   display: "flex"
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0"
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultRating: PropTypes.number,
+  className: PropTypes.string,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array
+  //   onSetRating: PropTypes.func
 };
 
-const starStyle = {
-  display: "block",
-  height: "48px",
-  width: "48px",
-  cursor: "pointer"
-};
-
-export default function StartRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  maxRating = 5,
+  defaultRating = 0,
+  className = "",
+  color = "#fcc419",
+  size = 48,
+  messages = []
+  //   onSetRating
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`
+  };
+
+  function onHandleRating(rating) {
+    setRating(rating);
+    // onSetRating(rating);
+  }
 
   return (
     <div style={containerStyle}>
@@ -32,19 +50,31 @@ export default function StartRating({ maxRating = 5 }) {
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRate={() => setRating(i + 1)}
+            onRate={() => onHandleRating(i + 1)}
             onRateInHover={() => setTempRating(i + 1)}
             onRateOutHover={() => setTempRating(0)}
             full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            size={size}
+            color={color}
           />
         ))}
       </div>
-      <span style={textStyle}>{tempRating || rating || ""}</span>
+      <span style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </span>
     </div>
   );
 }
 
-function Star({ onRate, onRateInHover, onRateOutHover, full }) {
+function Star({ onRate, onRateInHover, onRateOutHover, full, size, color }) {
+  const starStyle = {
+    display: "block",
+    height: `${size}px`,
+    width: `${size}px`,
+    cursor: "pointer"
+  };
   return (
     <span
       style={starStyle}
@@ -57,8 +87,8 @@ function Star({ onRate, onRateInHover, onRateOutHover, full }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={`${color}`}
+          stroke={`${color}`}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -67,7 +97,7 @@ function Star({ onRate, onRateInHover, onRateOutHover, full }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={`${color}`}
         >
           <path
             strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
-import StartRating from "./StarRating";
+import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {
       <>
-        <StartRating maxRating={5} />
+        <StarRating
+          maxRating={10}
+          messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+        />
       </>
       /* <App /> */
     }
