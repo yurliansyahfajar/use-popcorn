21714902518970b9c11f3a09143937cadd6abeeb
yurliansyahf <yurliansyahf@gmail.com>
2024-06-26 21:38:50 +0700

    create start component also hover and set rating function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StartRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {
+      <>
+        <StartRating maxRating={5} />
+      </>
+      /* <App /> */
+    }
   </React.StrictMode>
 );
